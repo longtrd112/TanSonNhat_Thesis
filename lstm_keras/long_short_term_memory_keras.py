eccb567fc5703474fb6f956971fbd38123bf61ba
longtrd112 <longtran011200@gmail.com>
2023-03-19 08:59:48 +0700

    1903

--- lstm_keras/long_short_term_memory_keras.py
+++ lstm_keras/long_short_term_memory_keras.py
@@ -14,7 +14,7 @@ with open("data_x_1.pkl", "rb") as f:
 with open("data_y_1.pkl", "rb") as f:
     data_y = pickle.load(f)
 
-for i in range(5, 10):
+for i in range(15, 20):
     # Split data into train and test sets
     train_x, test_x, train_y, test_y = train_test_split(data_x, data_y, test_size=0.2)
     train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size=0.25)
@@ -27,11 +27,11 @@ for i in range(5, 10):
     plateau = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, min_lr=0.000001)
 
     optimizer = tf.keras.optimizers.Adam(epsilon=10e-4, clipnorm=1)
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, min_delta=10, verbose=0)
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=6, min_delta=10, verbose=0)
     model.compile(loss=tf.keras.losses.mean_absolute_error, optimizer=optimizer,
                   metrics=[tf.keras.metrics.MeanAbsoluteError(), tf.keras.metrics.RootMeanSquaredError()])
     # Train the model
-    result = model.fit(train_x, train_y, epochs=30, batch_size=4096, validation_data=(val_x, val_y), verbose=1,
+    result = model.fit(train_x, train_y, epochs=30, batch_size=8192, validation_data=(val_x, val_y), verbose=1,
                        callbacks=[early_stopping, plateau])
 
     # Evaluate the model on test data
@@ -40,8 +40,21 @@ for i in range(5, 10):
     y_predict = model.predict(test_x, verbose=0)
 
     figure_name = f"figure_{i}.png"
+    plot_range = range(2, len(result.history['loss']))
     plt.plot(result.history['loss'], label='Training Loss')
     plt.plot(result.history['val_loss'], label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.xticks(range(1, len(result.history['loss']), 2))
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(figure_name, bbox_inches='tight')
+    plt.clf()
+
+    figure_name = f"figure_{i}_zoom.png"
+    plt.plot(plot_range, result.history['loss'][1:-1], label='Training Loss')
+    plt.plot(plot_range, result.history['val_loss'][1:-1], label='Validation Loss')
     plt.title('Training and Validation Loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
